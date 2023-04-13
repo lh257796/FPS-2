1910594cf23c46bb6f9785984a4a5334d4b20777
Han <hl2341@nyu.edu>
2023-04-13 17:25:57 -0400

    this entire operation has been botched. it is time to begin anew

--- Assets/Scripts/UIDisplay.cs
+++ Assets/Scripts/UIDisplay.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIDisplay : MonoBehaviour
 {
+    public Text fpsText;
+    private FPSCounter fpsCounter;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        fpsCounter = FindObjectOfType<FPSCounter>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float currentFPS = fpsCounter.GetFPS();
+        fpsText.text = $"FPS: {currentFPS}";
     }
 }
